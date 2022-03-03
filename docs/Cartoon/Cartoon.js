4d0bc2ff32c43525153e774f0754ef4ec05f1e70
AhmedS12899 <AhmedS12899@Gmail.com>
2022-03-03 17:12:51 -0500

    Edited cartoon

--- docs/Cartoon/Cartoon.js
+++ docs/Cartoon/Cartoon.js
@@ -1,26 +1,29 @@
 var canvas = document.getElementById("cartoon");
 var c = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 700;
+canvas.height = 700;
 
 
 //Drawing house
 //draw pentagon
 c.beginPath();
-c.moveTo(300,500); 
-c.lineTo(600,500);
-c.lineTo(600,300);
-c.lineTo(300*1.5,200);
-c.lineTo(300,300);
-c.lineTo(300,500);
+c.moveTo(0,450);
+c.lineTo(100,450);
+c.moveTo(600,450);
+c.lineTo(700,450);
+c.moveTo(300,480); 
+c.lineTo(600,480);
+c.lineTo(600,240);
+c.lineTo(300*1.5,140);
+c.lineTo(300,240);
+c.lineTo(300,480);
 c.strokeStyle = "black";
 c.fillStyle = "#E5E7D9";
 c.fill();
 c.stroke();
-
 //draw garage
-c.lineTo(100,500);
-c.lineTo(100,550*3/4);
-c.lineTo(300,550*3/4);
+c.lineTo(100,480);
+c.lineTo(100,480*3/4);
+c.lineTo(300,480*3/4);
 c.fill();
 c.stroke();
