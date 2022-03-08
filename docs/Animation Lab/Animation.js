4b14dcb2d805f09f77987f6501d402d17cfa9ded
AhmedS12899 <AhmedS12899@Gmail.com>
2022-03-08 08:30:50 -0500

    comit

--- docs/Animation Lab/Animation.js
+++ docs/Animation Lab/Animation.js
@@ -1,71 +1,71 @@
 "use strict";
 
 var Scene = {
-    canvas : undefined,
-    canvasContext : undefined,
+	canvas: undefined,
+	canvasContext: undefined,
 	sprite: undefined
 };
 
 Scene.start = function () {
 	// Get the canvas and it's context.
-    Scene.canvas = document.getElementById("myCanvas");
-    Scene.canvasContext = Scene.canvas.getContext("2d");
-	
+	Scene.canvas = document.getElementById("myCanvas");
+	Scene.canvasContext = Scene.canvas.getContext("2d");
+
 	// Seup the parrot to be displayed.
-    Scene.sprite = number;
-	
+	Scene.sprite = number;
+
 	// Attach the image to be used for the sprite.
 	Scene.sprite.img = new Image();
-    Scene.sprite.img.src = Scene.sprite.src;
-	
+	Scene.sprite.img.src = Scene.sprite.src;
+
 	// Wait till the parrot image is loaded before starting the animation.
-	Scene.sprite.img.onload = function() {		
-		Scene.sprite.offset=-Scene.sprite.frames[Scene.sprite.frame].frame.w;
-    	Scene.mainLoop();
+	Scene.sprite.img.onload = function () {
+		Scene.sprite.offset = -Scene.sprite.frames[Scene.sprite.frame].frame.w;
+		Scene.mainLoop();
 	}
 };
 
 // Once the basic HTML document is loaded and its parsing has taken place, start the scene.
-document.addEventListener( 'DOMContentLoaded', Scene.start);
+document.addEventListener('DOMContentLoaded', Scene.start);
 
 Scene.clearCanvas = function () {
-    Scene.canvasContext.fillStyle = "white";
-    Scene.canvasContext.fillRect(0, 0, Scene.canvas.width, Scene.canvas.height);
+	Scene.canvasContext.fillStyle = "white";
+	Scene.canvasContext.fillRect(0, 0, Scene.canvas.width, Scene.canvas.height);
 };
 
-Scene.mainLoop = function() {
-    Scene.clearCanvas();
-    Scene.update();
-    Scene.draw();
-	
+Scene.mainLoop = function () {
+	Scene.clearCanvas();
+	Scene.update();
+	Scene.draw();
+
 	// Animate at 24 frames a second.
-    window.setTimeout(Scene.mainLoop, 1000 /1);
+	window.setTimeout(Scene.mainLoop, 1000 / 1);
 };
 
 Scene.update = function () {
 	// Set the canvas width to be that of the display Window. Which helps if you resize the window.
-  	Scene.canvas.width = 500;
-	
+	Scene.canvas.width = 500;
+
 	// Set the location of the next frame. 
-  	Scene.sprite.offset=0;
-	if(Scene.sprite.offset>Scene.canvas.width)
- 		Scene.sprite.offset=-Scene.sprite.frames[Scene.sprite.frame].frame.w;
+	Scene.sprite.offset = 0;
+	if (Scene.sprite.offset > Scene.canvas.width)
+		Scene.sprite.offset = -Scene.sprite.frames[Scene.sprite.frame].frame.w;
 };
 
 Scene.draw = function () {
-	Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[Scene.sprite.frame].frame.x,Scene.sprite.frames[Scene.sprite.frame].frame.y,Scene.sprite.frames[Scene.sprite.frame].frame.w,Scene.sprite.frames[Scene.sprite.frame].frame.h,Scene.sprite.offset,0,Scene.sprite.frames[Scene.sprite.frame].frame.w,Scene.sprite.frames[Scene.sprite.frame].frame.h);
-	
+	Scene.canvasContext.drawImage(Scene.sprite.img, Scene.sprite.frames[Scene.sprite.frame].frame.x, Scene.sprite.frames[Scene.sprite.frame].frame.y, Scene.sprite.frames[Scene.sprite.frame].frame.w, Scene.sprite.frames[Scene.sprite.frame].frame.h, Scene.sprite.offset, 0, Scene.sprite.frames[Scene.sprite.frame].frame.w, Scene.sprite.frames[Scene.sprite.frame].frame.h);
+
 	// Advance to the next frame.
 	Scene.sprite.frame++;
 
 	// At the end of the sprite sheet, start at the first frame.
-	if(Scene.sprite.frame==Scene.sprite.frames.length)
-		Scene.sprite.frame=0;
+	if (Scene.sprite.frame == Scene.sprite.frames.length)
+		Scene.sprite.frame = 0;
 };
 console.log(number.frames[1]);
-for(var i = 0;i<=9;i++){
-	for(var j=0;j<=9;j++){
-		console.log({i,j});
+for (var i = 0; i <= 9; i++) {
+	for (var j = 0; j <= 9; j++) {
+		console.log({ i, j });
 	}
 }
 
